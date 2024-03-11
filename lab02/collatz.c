7aeec87e3c01d3657a4b59cb8b0227b67dea4a23
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2024-03-11 19:23:39 +0100

    lab02 init

---
+++ lab02/collatz.c
@@ -0,0 +1,5 @@
+//
+// Created by Kamil on 11/03/2024.
+//
+
+#include "collatz.h"
---
+++ lab02/collatz.h
@@ -0,0 +1,8 @@
+//
+// Created by Kamil on 11/03/2024.
+//
+
+#ifndef SYSOPY_COLLATZ_H
+#define SYSOPY_COLLATZ_H
+
+#endif //SYSOPY_COLLATZ_H
